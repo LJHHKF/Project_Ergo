163eb788f13002a3f81b91df53bc46402b3469e0
LJHHKF <43880473+LJHHKF@users.noreply.github.com>
2021-03-17 14:53:29 +0900

    Update AbCondInfoManager.cs

--- Assets/Script/Battle/LivingEntity/AbCondInfoManager.cs
+++ Assets/Script/Battle/LivingEntity/AbCondInfoManager.cs
@@ -18,6 +18,9 @@ public class AbCondInfoManager : MonoBehaviour
     private static AbCondInfoManager m_instance;
 
     public Sprite[] conditions_img;
+    [TextArea]
+    public string[] conditions_text; // 효과치가 계산되어 들어갈 자리엔 '(효과치)'라고 입력할 것.
+    public int[] conditions_OnePower; // 1중첩당 효과 증감 수치
 
     private void Awake()
     {
