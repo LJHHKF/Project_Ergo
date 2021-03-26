6a0f3e92161c58af14fb575aca2bf1aaa9c01a0a
LJHHKF <43880473+LJHHKF@users.noreply.github.com>
2021-03-26 23:03:22 +0900

    CardPack 및 초기 카드 설정 작성 완료

---
+++ Assets/Script/Battle/Card/Decks/CardPack.cs
@@ -0,0 +1,168 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Text;
+
+public class CardPack : MonoBehaviour
+{
+    [Header("Pack Settincg")]
+    public int max_DuplicateValue = 3;
+    private int saveID = 0;
+
+    [Header("Card Registration")]
+    public GameObject[] card_prefabs;
+    public float[] card_weight;
+    private int[] cardIDs;
+    private int[] card_HadCnt;
+
+    private void OnApplicationQuit()
+    {
+        SaveHadCnt();
+    }
+
+    public void CardPackInit()
+    {
+        cardIDs = new int[card_prefabs.Length];
+        card_HadCnt = new int[card_prefabs.Length];
+
+        for (int i = 0; i < card_prefabs.Length; i++)
+        {
+            cardIDs[i] = card_prefabs[i].GetComponent<ICard>().GetCardID();
+
+            StringBuilder key = new StringBuilder();
+            key.Append("saveID(");
+            key.Append(saveID.ToString());
+            key.Append(").cardID(");
+            key.Append(cardIDs[i].ToString());
+            key.Append(").hadCnt");
+
+            if (PlayerPrefs.HasKey(key.ToString()) == false)
+            {
+                card_HadCnt[i] = 0;
+                PlayerPrefs.SetInt(key.ToString(), card_HadCnt[i]);
+            }
+            else
+            {
+                card_HadCnt[i] = PlayerPrefs.GetInt(key.ToString());
+            }
+        }
+    }
+
+    //public int CardsArrayLength()
+    //{
+    //    return card_prefabs.Length;
+    //}
+
+    public void AddCard_OnlyHadData(int c_id)
+    {
+        int index = -1;
+        for(int i = 0; i< cardIDs.Length;i++)
+        {
+            if(cardIDs[i] == c_id)
+            {
+                index = c_id;
+                break;
+            }
+        }
+
+        if(index == -1)
+        {
+            Debug.LogError("추가할 카드의 아이디와 일치하는 카드를 발견하지 못함");
+            return;
+        }
+
+        if (card_HadCnt[index] < 3)
+        {
+            card_HadCnt[index] += 1;
+        }
+        else
+        {
+            Debug.Log("소지 카드수가 이미 3을 넘었습니다.");
+        }
+    }
+
+    public void AddCard_Object(int c_id, Transform _p, ref List<GameObject> _out)
+    {
+        int index = -1;
+        for (int i = 0; i < cardIDs.Length; i++)
+        {
+            if (cardIDs[i] == c_id)
+            {
+                index = c_id;
+                break;
+            }
+        }
+
+        if (index == -1)
+        {
+            Debug.LogError("추가할 카드의 아이디와 일치하는 카드를 발견하지 못함");
+            return;
+        }
+
+        if (card_HadCnt[index] < 3)
+        {
+            card_HadCnt[index] += 1;
+            GameObject go = Instantiate(card_prefabs[index], _p);
+            _out.Add(go);
+        }
+        else
+        {
+            Debug.Log("소지 카드수가 이미 3을 넘었습니다.");
+        }
+    }
+
+    public void InstantiateCards(Transform _p, ref List<GameObject> _out)
+    {
+        for(int i = 0; i < card_prefabs.Length; i++)
+        {
+            if(card_HadCnt[i] > 0)
+            {
+                for(int j = 0; j < card_HadCnt[i]; j++)
+                {
+                    GameObject go = Instantiate(card_prefabs[i], _p);
+                    _out.Add(go);
+                }
+            }
+        }
+    }
+
+    public void SetSaveID(int id)
+    {
+        saveID = id;
+    }
+
+    private void SaveHadCnt()
+    {
+        for(int i = 0; i < card_HadCnt.Length; i++)
+        {
+            StringBuilder key = new StringBuilder();
+            key.Append("saveID(");
+            key.Append(saveID.ToString());
+            key.Append(").cardID(");
+            key.Append(cardIDs[i].ToString());
+            key.Append(").hadCnt");
+            PlayerPrefs.SetInt(key.ToString(), card_HadCnt[i]);
+        }
+    }
+
+    public void DeleteSavedCardData(int s_ID)
+    {
+        for (int i = 0; i < card_HadCnt.Length; i++)
+        {
+            StringBuilder key = new StringBuilder();
+            key.Append("saveID(");
+            key.Append(saveID.ToString());
+            key.Append(").cardID(");
+            key.Append(cardIDs[i].ToString());
+            key.Append(").hadCnt");
+            PlayerPrefs.DeleteKey(key.ToString());
+        }
+
+        //이하 코드는 세이브 매니저 만들면 옮길 것
+        StringBuilder Key2 = new StringBuilder();
+        Key2.Append("SaveID(");
+        Key2.Append(saveID.ToString());
+        Key2.Append(")");
+        PlayerPrefs.SetInt(Key2.ToString(), 0); //false
+    }
+}
---
+++ Assets/Script/Battle/Card/Decks/InitCardSetting.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Text;
+
+public class InitCardSetting : MonoBehaviour
+{
+    [Header("Temp Set Value")]
+    public int saveID = 0;
+    
+    [Header("Card Init Set Value")]
+    public CardPack m_cardPack;
+    public int[] cardIDs;
+    public int[] cardHadValue;
+
+    private void Awake()
+    {
+        //추후엔 이 부분을 통째로 함수화한 후 세이브 매니저에서 불러올 것.
+        m_cardPack.SetSaveID(saveID);
+        m_cardPack.CardPackInit();
+
+        StringBuilder Key2 = new StringBuilder();
+        Key2.Append("SaveID(");
+        Key2.Append(saveID.ToString());
+        Key2.Append(")");
+
+        if (PlayerPrefs.GetInt(Key2.ToString()) == 0 || PlayerPrefs.HasKey(Key2.ToString()) == false)
+        {
+            PlayerPrefs.SetInt(Key2.ToString(), 1);
+            OnInitCardsSetting();
+        }
+    }
+
+    private void OnInitCardsSetting()
+    {
+        for(int i = 0; i < cardHadValue.Length; i++)
+        {
+            if (cardHadValue[i] > 0)
+            {
+                for (int j = 0; j < cardHadValue[i]; j++)
+                {
+                    m_cardPack.AddCard_OnlyHadData(cardIDs[i]);
+                }
+            }
+        }
+    }
+}
