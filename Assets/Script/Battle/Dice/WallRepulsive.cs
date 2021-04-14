a5f122405bfc44b40856cc9d31bdacfafc648205
LJHHKF <43880473+LJHHKF@users.noreply.github.com>
2021-04-15 06:19:07 +0900

    주사위 벽에 반발력 추가

---
+++ Assets/Script/Battle/Dice/WallRepulsive.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WallRepulsive : MonoBehaviour
+{
+    [SerializeField] private float x_power;
+    [SerializeField] private float z_power;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        Rigidbody t_rb = collision.gameObject.GetComponent<Rigidbody>();
+
+        t_rb.AddForce(new Vector3(x_power, 0, z_power));
+    }
+}
